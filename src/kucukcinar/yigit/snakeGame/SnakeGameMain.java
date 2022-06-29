9911ce3a16e9b1fea8758ee9aaf48e47eb4deb67
ykucukcinar <yigitkucukcinar@hotmail.com>
2022-06-29 22:43:18 +0300

    Snake game complete

--- src/kucukcinar/yigit/snakeGame/SnakeGameMain.java
+++ src/kucukcinar/yigit/snakeGame/SnakeGameMain.java
@@ -1,4 +1,18 @@
 package kucukcinar.yigit.snakeGame;
 
+import javax.swing.*;
+import java.awt.*;
+
 public class SnakeGameMain {
+	public static void main(String[] args) {
+		JFrame obj = new JFrame();
+		GamePlay gameplay = new GamePlay();
+
+		obj.setBounds(10,10,905,700);
+		obj.setBackground(Color.DARK_GRAY);
+		obj.setResizable(false);
+		obj.setVisible(true);
+		obj.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		obj.add(gameplay);
+	}
 }
